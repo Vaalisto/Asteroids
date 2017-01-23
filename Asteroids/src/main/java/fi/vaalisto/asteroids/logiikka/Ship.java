14d9ba9cace8a0cb2241f1bdf693bc4a749d485b
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-01-23 23:12:32 +0200

    Luotu Ship-luokan konstruktori ja siihen Javadocia

--- Asteroids/src/main/java/fi/vaalisto/asteroids/logiikka/Ship.java
+++ Asteroids/src/main/java/fi/vaalisto/asteroids/logiikka/Ship.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fi.vaalisto.asteroids.logiikka;
 
 /**
@@ -13,7 +8,27 @@ public class Ship extends GameObj {
     boolean accelerating; // totuusarvo siitä kiihdyttääkö alus. Alus on ainoa objekti, joka voi kiihdyttää.
     boolean turningLeft, turningRight; // totuusarvo siitä kääntyykö alus. Alus on ainoa objekti, joka voi kääntyä.
     
-
+    /**
+     * Ship-luokan konstruktori. Nämä jätetään muuttujiksi ja aluksen speksejä voidaan muuttaa pelin toteuttavassa luokassa sen sijaan, että kovakoodattaisiin tähän luokkaan.
+     * @param x aluksen aloituspaikan x-koordinaatti
+     * @param y aluksen aloituspaikan y-koordinaatti
+     * @param angle aluksen keulan kulma aluksi
+     * @param xVelocity aluksen nopeuden x-komponentti alussa
+     * @param yVelocity aluksen nopeuden y-komponentti alussa
+     * @param acceleration aluksen kiihtyvyys
+     */
+    
+    public Ship(double x, double y, double angle, double xVelocity, double yVelocity, double acceleration) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+        this.acceleration = acceleration;
+        this.accelerating = false;
+        this.turningLeft = false;
+        this.turningRight = false;
+    }
     
 
 }
