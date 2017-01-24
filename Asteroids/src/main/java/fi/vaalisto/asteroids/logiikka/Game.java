b72148d730c4cad0b8255ee4fecbe85e8a73ae64
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-01-24 10:51:15 +0200

    Musta tausta ikkunaan

--- Asteroids/src/main/java/fi/vaalisto/asteroids/logiikka/Game.java
+++ Asteroids/src/main/java/fi/vaalisto/asteroids/logiikka/Game.java
@@ -20,6 +20,7 @@ public class Game extends JFrame {
         this.setSize(800, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
+        this.getContentPane().setBackground(Color.BLACK);
         
     }
 }
