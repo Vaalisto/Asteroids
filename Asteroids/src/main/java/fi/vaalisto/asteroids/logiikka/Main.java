a7709bc25602d9e7abeac04cdcc5c855a5ca58b4
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-02-17 16:19:18 +0200

    Säie ei toimi

--- Asteroids/src/main/java/fi/vaalisto/asteroids/logiikka/Main.java
+++ Asteroids/src/main/java/fi/vaalisto/asteroids/logiikka/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Game game = new Game();
-        game.setVisible(true);
+        game.setVisible(true);        
     }
 
 }
